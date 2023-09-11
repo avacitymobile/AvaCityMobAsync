39ffd9fa73965292df4c617e089bf04fb4247899
avacitymobile <144659404+avacitymobile@users.noreply.github.com>
2023-09-11 13:47:05 +0300

    Add files via upload

--- const.py
+++ const.py
@@ -1,22 +1,21 @@
-
 USERS_IN_LOCATION: int = 20
 BUY_VIP: bool = False
 USER_RATING_COUNT: int = 10
 BUY_ROOM: bool = True
 RESTORE_ENERGY: bool = True
 
-AVARDS: tuple = ("vip", "crafter", "magnate", "generous", 
+AWARDS: tuple = ("vip", "crafter", "magnate", "generous", 
                  "pilowChamp", "workaholic", "goodFriend", 
                  "shopaholic", "beachRaceChamp", "schoolMaster")
 
-
 class ROLE:
-    AVATAR_USER_ROLE:      int = 1
-    AVATAR_PREMIUM_ROLE:   int = 2
+    AVATAR_USER_ROLE: int = 1
+    AVATAR_PREMIUM_ROLE: int = 2
     AVATAR_MODERATOR_ROLE: int = 3 
-    AVATAR_ADMIN_ROLE:     int = 4
+    AVATAR_ADMIN_ROLE: int = 4
 
-
-NEW_ROOM = [{"tpid": "wall15", "d": 3, "lid": 1, "x": 0.0, "y": 0.0, "z": 0.0},
-            {"tpid": "wall15", "d": 5, "lid": 2, "x": 13.0, "y": 0.0, "z": 0.0},
-            {"tpid": "floor4", "d": 5, "lid": 3, "x": 0.0, "y": 0.0, "z": 0.0}]
+NEW_ROOM = [
+    {"tpid": "wall15", "d": 3, "lid": 1, "x": 0.0, "y": 0.0, "z": 0.0},
+    {"tpid": "wall15", "d": 5, "lid": 2, "x": 13.0, "y": 0.0, "z": 0.0},
+    {"tpid": "floor4", "d": 5, "lid": 3, "x": 0.0, "y": 0.0, "z": 0.0}
+]
